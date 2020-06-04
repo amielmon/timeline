8a591c2ce951c5f97ebf4551d2f67767ad7da4c4
Amiel Monasterial <amielmon@gmail.com>
2020-06-04 00:21:01 -0400

    poc

---
+++ components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './style.module.css'
+
+function Header() {
+  
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>A Timeline</span>
+    </div>
+  )
+}
+
+
+
+
+
+export default Header
