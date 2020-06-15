4fb530faea651a5f7fc35104354126760704256e
Amiel Monasterial <amielmon@gmail.com>
2020-06-15 19:57:03 -0400

    click outside

--- components/Resources/ResourceModal/index.js
+++ components/Resources/ResourceModal/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import styles from './style.module.css'
 import InstagramEmbed from 'react-instagram-embed';
 
 export default function ResourceModal({resource, handleClose, show}) {
 
+  const modalRef = useRef(null)
+
+  // Click outside to close
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modalRef.current.contains(e.target)) {
+        return;
+      }
+      handleClose(false);
+    }
+
+    if (show) {
+      document.addEventListener('mousedown', handleClickOutside)
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    };
+  }, [show, modalRef, handleClose])
+
   if (show) {
     return (
-      <div className={styles.container}>
+      <div className={styles.container} ref={modalRef}>
         <div className={styles.content}>
           <img className={styles.close} src={require('../../../public/photos/close.png')} onClick={handleClose}/>
           <div className={styles.resource} >
