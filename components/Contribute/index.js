5de421aad89343d0e46f47b5c0a0134f26342ce8
Amiel Monasterial <amielmon@gmail.com>
2020-06-17 00:37:34 -0400

    start form

--- components/Contribute/index.js
+++ components/Contribute/index.js
@@ -1,5 +1,9 @@
 import React, {useState} from 'react'
 import styles from './style.module.css'
+import User from '../Icons/User'
+import Email from '../Icons/Email'
+import Timeline from '../Icons/Timeline'
+import Resource from '../Icons/Resource'
 
 function Contribute() {
   return (
@@ -8,10 +12,22 @@ function Contribute() {
         <div className={styles.formContainer}>
           <div className={styles.form}>
             <div className={styles.formTitle}>Submit events or resources</div>
-            <div className={styles.name}>
-              <input></input>
-            </div>
-            
+            <div className={styles.inputWrapper}>
+                <User fill={'#9d96a8'}/>
+                <input placeholder='Enter name (optional)'/>
+              </div>
+              <div className={styles.inputWrapper}>
+                <Email fill={'#9d96a8'}/>
+                <input placeholder='Enter email (optional)'/>
+              </div>
+              <div className={styles.inputWrapper}>
+                <Timeline fill={'#9d96a8'}/>
+                <input placeholder='Event links'/>
+              </div>
+              <div className={styles.inputWrapper}>
+                <Resource fill={'#9d96a8'}/>
+                <input placeholder='Resource links'/>
+              </div>
           </div>
         </div>
     </div>
---
+++ components/Icons/Email.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default ({ width='28', height='28', fill, className }) => (
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" width={width} height={height} className={className} x="0px" y="0px"
+	 viewBox="0 0 477.867 477.867">
+<g>
+	<g>
+		<path d="M460.8,68.267H17.067l221.867,182.75L463.309,68.779C462.488,68.539,461.649,68.368,460.8,68.267z" fill={fill}/>
+	</g>
+</g>
+<g>
+	<g>
+		<path d="M249.702,286.31c-6.288,5.149-15.335,5.149-21.623,0L0,98.406v294.127c0,9.426,7.641,17.067,17.067,17.067H460.8
+			c9.426,0,17.067-7.641,17.067-17.067V100.932L249.702,286.31z" fill={fill}/>
+	</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
+
+)
