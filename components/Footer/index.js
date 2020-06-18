7dc1d81cacd9ce5dc2f806b5d0618cb92cfca313
Angela Gomba <angelamariegomba@gmail.com>
2020-06-17 21:46:12 -0700

    update footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,37 +8,28 @@ import styles from './style.module.css'
 export default function Footer() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.poopee}>
-        <span className={styles.name}>Angela Gomba</span>
-        <div className={styles.info}>
-          <span className={styles.content}>
+      <span className={styles.message}>Made with love by Angela Gomba and Amiel Monasterial</span>
+      <span className={styles.info}>
+        <span className={styles.contact}>Contact</span>
+        <span className={styles.text}>
+          angelamariegomba@gmail.com
+          <a href='https://github.com/angelagomba'>
             <Github className={styles.github}/>
+          </a>
+          <a href='https://www.linkedin.com/in/angela-gomba/'>
             <LinkedIn className={styles.linkedIn}/>
-            <Email />
-          </span>
-          <span className={styles.content}>
-            <a className={styles.text} href='https://github.com/angelagomba'>Github</a>
-            <a className={styles.text} href='https://www.linkedin.com/in/angela-gomba/'>LinkedIn</a>
-            <span className={styles.text}>angelamariegomba@gmail.com</span>
-          </span>
-        </div>
-      </div>
-      <div className={styles.poopee}>
-        <span className={styles.name}>Amiel Monasterial</span>
-        <div className={styles.info}>
-          <span className={styles.content}>
+          </a>
+        </span>
+        <span className={styles.text}>
+          amielmon@gmail.com
+          <a href='https://github.com/angelagomba'>
             <Github className={styles.github}/>
+          </a>
+          <a href='https://www.linkedin.com/in/angela-gomba/'>
             <LinkedIn className={styles.linkedIn}/>
-            <Email />
-          </span>
-          <span className={styles.content}>
-            <a className={styles.text} href='https://github.com/amielmon'>Github</a>
-            <a className={styles.text} href='https://www.linkedin.com/in/amielmon'>LinkedIn</a>
-            <span className={styles.text}>amielmon@gmail.com</span>
-          </span>
-        </div>
-      </div>
+          </a>
+        </span>
+      </span>
     </div>
-
   )
 }
