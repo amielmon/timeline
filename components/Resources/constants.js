068db55c80e68d3f3027c5980d128fa97f0b7b2f
Angela Gomba <angelamariegomba@gmail.com>
2020-06-18 22:01:27 -0700

    add resources and fix styles

--- components/Resources/constants.js
+++ components/Resources/constants.js
@@ -2,4 +2,7 @@ export const IMAGE_PATH = Object.freeze({
   resource0: require('../../public/photos/resource-0.png'),
   resource1: require('../../public/photos/resource-1.png'),
   resource2: require('../../public/photos/resource-2.png'),
+  resource3: require('../../public/photos/resource-3.png'),
+  resource4: require('../../public/photos/resource-4.png'),
+  resource5: require('../../public/photos/resource-5.png'),
 })
