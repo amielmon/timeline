16159aecf2b6af9f3c75e217725f087a7c9da59f
Amiel Monasterial <amielmon@gmail.com>
2020-06-20 16:17:39 -0400

    eee

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,8 @@ export default function Home({resEvents, resResources}) {
   resEvents.sort((a,b) => new Date(a.date) - new Date(b.date))
 
   const [ref, inView] = useInView({threshold: 0.5})
+  
+  console.log('ga', process.env.GA_TRACKING_ID)
 
   return (
     <body id='page' className={styles.page}>
