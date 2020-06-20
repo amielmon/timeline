9ae4ed50c5f014fa92094d0c2d8ccb5426924e89
Amiel Monasterial <amielmon@gmail.com>
2020-06-20 15:35:07 -0400

    attempt

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,24 @@ export default function Home({resEvents, resResources}) {
   return (
     <body id='page' className={styles.page}>
       <Head>
-      <title>Timeline of a Revolution</title>
-      <meta name="viewport" content="initial-scale=0.5, width=device-width" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+          }}
+        />
+        <title>Timeline of a Revolution</title>
+        <meta name="viewport" content="initial-scale=0.5, width=device-width" />
       </Head>      
       <Header />
       <div className={inView ? styles.about : styles.aboutOut} ref={ref}>
